234ee234f12a5e372df359c9483e8fe3cec5ff8a
Yuri Popov <your.ppv@mail.ru>
2025-11-22 14:25:36 +0300

    Initial project version

---
+++ channel_funcs.py
@@ -0,0 +1,13 @@
+import numpy as np
+import commpy.modulation
+
+
+def main():
+    ### PBRS
+    bits_num = 12
+    bits  = np.random.randint(0, 2, bits_num)
+    
+    return 1
+
+if __name__ == '__main__':
+    main()
